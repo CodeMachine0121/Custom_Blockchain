82fb10e310ed19e48fc153e373bfbd3455fa5420
CodeMachine0121 <CodeMachine0121@gmail.com>
2020-09-10 20:49:05 +0800

    加密解密 使用者 Certificate

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -35,14 +35,17 @@ public class test {
         System.out.println("private key:\t"+ privatekey);
 
 
+
         String str= "hello";
         String cipherText = KeyGenerater.RSA_Encrypt(str,KeyGenerater.Get_RSA_PublicKey(publickey));
         System.out.println("加密:\t"+cipherText);
         System.out.println("解密:\t"+KeyGenerater.RSA_Decrypt(cipherText,KeyGenerater.Get_RSA_PrivateKey(privatekey)));
 
 
-        String t= "oB/sPgZDvGXaeTdBuf+aBcZtoyorYXmHL83KWYoOBfqJrKpRPuNhzyYcV7W7UMhS6hW/iTT532KAZeRCaHZ3MQ==0qXJKeBGD9V1Ovf6nwFeL2kWgsubMJ/4oR5JiMIUb7BvSAWw+0PnIDF0JTMA69UomEsHeORgJ0hzLrMLmRJStw==toZQ2V0DOqF4SvfYyUS0JllxSJ+HftRENcj7+px45/8wfULLFSVGs6aSyLnsqwd8M22vBKmzysgTJJOxjbwHVw==IkZpArVTcrG5qQV202gEeJeLA2z/Vz77QoOiNq13e/Jv6oU08Rs/2+/NpjNmR7xGRacH6DRSikLDsw4gtscaQg==Wpij2QmmYgI94mQM0x8eVPWHqQVGusWJ8VsP06D3fiJkd8Ax4WF0R63O1K6azQjGw9YJTtT/l8IkdIYd5O4xvA==l0B+oacrujP+MzSp+G/nBL2K9nMEt6C5OLL9hZz7Frsn7Wdg1fuNoF6CrSlr/ukXLATe4K4O66Rl/fPBTqp2JA==";
-        System.out.println(KeyGenerater.RSA_Decrypt(t,KeyGenerater.Get_RSA_PrivateKey("MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEA2wZ56eGDmkSLeekjOCYS07L8gKZWczRo0gc7kpXyoUPoLvNFe6HsqNPFmg40q1+NRhWQMhuyUNwgrkXvshTBYwIDAQABAkAX1PWRAEvMjmbKxhZh9qqXxGL7MJ45fNtm9wiBY7V53HEvGrU8BfDxYzYPF6y6+zG1oHNykOao9ICY4BbT06g5AiEA7+crRdUQlxNaLawFYXCnV+3HbkPPwnC5+6XSUZsUPS8CIQDpuLB45Kruk362CebxN8dMHJkyKCd9pe8u7YCWhJ86DQIhAOC+nBdrp4MgqhanVNMYCm2hYHe4J2Zs3I8XpZS1x5wZAiAUuEzcytjkgarwFHym0d0XQEnMiobFutozfNazgVpkWQIgCeddImXGnHO+76SywlgGrtLkQpbAPANYIYT7JB6j8nI=")));
+        String t= "x00nx5dizhpNFE0y9JudlXDFqPGIoLNwZXc2cP4ttbn8xIOxrwHOLLR8OYwIdXFA/u/CqO1DyY/gHSMbASOZZA==ZZ7bPi6b5r88awv5XnNzEZScyRidmynkozEHOBemoktk7zK52zm/yxSmalBnN1KVbPIMjwzIsV1r9mXc5Li0QA==kGcVcvxLmqJnYwh+0o9tNvVsl+nS0ZfBzhZ0UkkMH8N0sQqaZYbGXLKRWAtlY+CrvJqoF4L+cNCkS0snTtYetQ==HweD63SAeCW+MLqzJmsnewl0MvIz5m89IVFx9r7eXpzccejqW6R6hXAeljnd+4J2Ij3ACvuFe7m2kKtyhSD2oA==fjIqW9U1Mf0cVxvWRrB3+e+NXt7JKmmoRAc3tOZlmOz5flB1aCm6c+OMCWgjqj5/GScY0rbHnmUm486tioUqQw==0m51CHst/hRIRhcT/+vRoejCD2N/cFp8MwxkU2Ay1E1iDdMOCg7VCnQAKW5WxAMFitNf0DaVmlv8X7ham7tr3w==";
+
+        System.out.println("解密:\t"+KeyGenerater.RSA_Decrypt(t,KeyGenerater.Get_RSA_PrivateKey("MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEA2wZ56eGDmkSLeekjOCYS07L8gKZWczRo0gc7kpXyoUPoLvNFe6HsqNPFmg40q1+NRhWQMhuyUNwgrkXvshTBYwIDAQABAkAX1PWRAEvMjmbKxhZh9qqXxGL7MJ45fNtm9wiBY7V53HEvGrU8BfDxYzYPF6y6+zG1oHNykOao9ICY4BbT06g5AiEA7+crRdUQlxNaLawFYXCnV+3HbkPPwnC5+6XSUZsUPS8CIQDpuLB45Kruk362CebxN8dMHJkyKCd9pe8u7YCWhJ86DQIhAOC+nBdrp4MgqhanVNMYCm2hYHe4J2Zs3I8XpZS1x5wZAiAUuEzcytjkgarwFHym0d0XQEnMiobFutozfNazgVpkWQIgCeddImXGnHO+76SywlgGrtLkQpbAPANYIYT7JB6j8nI=")));
+
     }
 
 
